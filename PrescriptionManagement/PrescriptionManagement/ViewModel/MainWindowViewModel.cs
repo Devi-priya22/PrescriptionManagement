db79a25b9d0b67b710a1a657d5843d174d43c689
Keerthana V A <154310420+keethzz@users.noreply.github.com>
2024-12-16 17:41:35 +0530

    ViewModel second commit

--- PrescriptionManagement/PrescriptionManagement/ViewModel/MainWindowViewModel.cs
+++ PrescriptionManagement/PrescriptionManagement/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace PrescriptionManagement.ViewModel
 {
-    internal class MainWindowViewModel:ViewModelBase
+    internal class MainWindowViewModel:ViewModelBase,IDataErrorInfo
     {
 		private string _name;
 
@@ -20,7 +23,7 @@ namespace PrescriptionManagement.ViewModel
 
 
 		private int _age;
-
+		[Range(10,20)]
 		public int age
 		{
 			get { return _age; }
@@ -44,10 +47,34 @@ namespace PrescriptionManagement.ViewModel
 		public DateTime Date
 		{
 			get { return _date; }
-			set { _date = value; }
+			set { _date = value;
+				OnPropertyChanged();
+			}
 		}
 
+		private string _search;
 
+		public string Search
+		{
+			get { return _search; }
+			set { _search = value;
+				OnPropertyChanged();
+			}
+		}
 
+        public string Error { get; }
+
+        public string this[string columnName]
+		{
+            get
+            {
+                var context = new ValidationContext(this) { MemberName = columnName };
+                var results = new List<ValidationResult>();
+                var propInfo = this.GetType().GetProperty(columnName);
+                var value = propInfo.GetValue(this);
+                var isValid = Validator.TryValidateProperty(value, context, results);
+                return isValid ? null : results.First().ErrorMessage;
+            }
+        }
 	}
 }
