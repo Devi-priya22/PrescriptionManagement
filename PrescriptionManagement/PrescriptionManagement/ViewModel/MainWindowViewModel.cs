209d9d264f0da336b664d81e579cbd8584b545f5
Keerthana V A <154310420+keethzz@users.noreply.github.com>
2024-12-16 15:28:05 +0530

    view model commit

---
+++ PrescriptionManagement/PrescriptionManagement/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrescriptionManagement.ViewModel
+{
+    internal class MainWindowViewModel:ViewModelBase
+    {
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; 
+				OnPropertyChanged();
+			}
+		}
+
+
+		private int _age;
+
+		public int age
+		{
+			get { return _age; }
+			set { _age = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private string _gender;
+
+		public string Gender
+		{
+			get { return _gender; }
+			set { _gender = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private DateTime _date;
+
+		public DateTime Date
+		{
+			get { return _date; }
+			set { _date = value; }
+		}
+
+
+
+	}
+}
