be2353f3598b19ad35594d5e059eb68740d68652
Keerthana V A <154310420+keethzz@users.noreply.github.com>
2024-12-17 09:53:19 +0530

    Update RelayCommand.cs

--- PrescriptionManagement/PrescriptionManagement/Commands/RelayCommand.cs
+++ PrescriptionManagement/PrescriptionManagement/Commands/RelayCommand.cs
@@ -9,19 +9,32 @@ namespace PrescriptionManagement.Commands
 {
     internal class RelayCommand : ICommand
     {
-          
 
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
 
-        public event EventHandler CanExecuteChanged;
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return _canExecute is null ? true : _canExecute();
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            _execute();
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+        protected virtual void OnCanExecute()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
